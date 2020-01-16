1ff011894f52800cfb1ac7949d501ca62b737ae1
preetgur <preetgur0137@gmail.com>
2020-01-16 11:02:26 +0530

    First version of Caulator

---
+++ calculator.py
@@ -0,0 +1,7 @@
+print("#### Welcome to the calculator #######")
+
+x = int(input("Please Enter First Number : "))
+y = int(input("Please Enter Second Number : "))
+
+print(f"You enter {x} and {y} as two values : ")
+
